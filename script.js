57159af5b588eba11f9ac8a11411f51979072330
Manouchehr <e1129004@student.tuwien.ac.at>
2021-04-19 09:12:55 +0200

    update is done

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+	const amount = Number(document.getElementById('amount').value);
+	const rate = Number(document.getElementById('rate').value);
+	const years = Number(document.getElementById('years').value);
+	const result = document.getElementById('result');
+	
+	var year = 2020 + years;
+	var sum = years * amount * (rate/100); 
+	
+	var x = "<p>If you deposit <span>" + amount + "</span>,<br>" +
+	"at an interest of <span>" + rate + "%</span><br>" +
+	"You will receive an amount of <span>" + parseInt(sum) + "</span>,<br>" +
+	"in the year <span>" + year + "</span></p>";
+	result.innerHTML = x;
+   
 }
-        
+window.onload = function(){
+	const rate = document.getElementById('rate');
+	const output = document.getElementById('rate-output');
+	output.textContent = rate.value;
+	rate.addEventListener('input', function() {
+	  output.textContent = rate.value + "%";
+	});
+}
