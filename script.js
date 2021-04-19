446c80905a295ceadea221a3127cc7224c758f0e
Manouchehr <e1129004@student.tuwien.ac.at>
2021-04-19 09:26:42 +0200

    update 2 is done

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ function compute(){
 	const years = Number(document.getElementById('years').value);
 	const result = document.getElementById('result');
 	
-	var year = 2020 + years;
-	var sum = years * amount * (rate/100); 
-	
-	var x = "<p>If you deposit <span>" + amount + "</span>,<br>" +
-	"at an interest of <span>" + rate + "%</span><br>" +
-	"You will receive an amount of <span>" + parseInt(sum) + "</span>,<br>" +
-	"in the year <span>" + year + "</span></p>";
-	result.innerHTML = x;
+	if(amount <= 0){
+
+		alert("Please enter a positive value for Amount field!");
+		document.getElementById('amount').focus();
+
+	}else{
+		
+		var year = 2020 + years;
+		var sum = years * amount * (rate/100); 
+		
+		var x = "<p>If you deposit <span>" + amount + "</span>,<br>" +
+		"at an interest of <span>" + rate + "%</span><br>" +
+		"You will receive an amount of <span>" + parseInt(sum) + "</span>,<br>" +
+		"in the year <span>" + year + "</span></p>";
+		result.innerHTML = x;
+		
+	}
    
 }
 window.onload = function(){
